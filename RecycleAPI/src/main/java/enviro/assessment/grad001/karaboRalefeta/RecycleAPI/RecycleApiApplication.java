685ba4f524603a02a70b08c870ec0182e9860f64
karabo <karabo.ralefeta07@gmail.com>
2025-01-25 20:31:31 +0200

    Setup project

---
+++ RecycleAPI/src/main/java/enviro/assessment/grad001/karaboRalefeta/RecycleAPI/RecycleApiApplication.java
@@ -0,0 +1,13 @@
+package enviro.assessment.grad001.karaboRalefeta.RecycleAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecycleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecycleApiApplication.class, args);
+	}
+
+}
---
+++ RecycleAPI/src/test/java/enviro/assessment/grad001/karaboRalefeta/RecycleAPI/RecycleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package enviro.assessment.grad001.karaboRalefeta.RecycleAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecycleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
